faa6663485b3204e6dbd6081073928f9f0e18f71
cornbeef111 <edembaonyekachi@icloud.com>
2023-11-02 17:13:07 +0100

    changed image

---
+++ js/style.js
@@ -0,0 +1,8 @@
+let text = document.getElementById('txthide');
+let text1 = document.getElementById('txthide1');
+let show = document.getElementById('hide')
+let show1 = document.getElementById('hide1')
+document.addEventListener('DOMContentLoaded',function(){
+  show.style.display ="none"; 
+  show1.style.display ="none"; 
+})

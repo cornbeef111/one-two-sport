1283d92e53575e2319f783fc6a683f605bee0797
cornbeef111 <edembaonyekachi@icloud.com>
2023-11-03 10:00:32 +0100

    final carousel change

--- js/style.js
+++ js/style.js
@@ -2,7 +2,22 @@ let text = document.getElementById('txthide');
 let text1 = document.getElementById('txthide1');
 let show = document.getElementById('hide')
 let show1 = document.getElementById('hide1')
+
+
 document.addEventListener('DOMContentLoaded',function(){
   show.style.display ="none"; 
   show1.style.display ="none"; 
+})
+
+document.addEventListener('click',function(){
+	if(true){
+		show.style.display="block";
+		text.style.display="none";
+	}
+})
+document.addEventListener('click',function(){
+	if(true){
+		show1.style.display="block";
+		text1.style.display="none";
+	}
 })

2101ee9c922a0e28ca3f352dee453ef81100b7dd
cornbeef111 <edembaonyekachi@icloud.com>
2023-11-21 16:30:12 +0100

    befor final touch

--- js/style.js
+++ js/style.js
@@ -52,6 +52,32 @@ setTimeout(() => writeUp.style.display='block',1000);
 setTimeout(()=> {writeUp.style.opacity = "1";
                  writeUp.style.transform='translateX(0)';},2000);
 
+// for the hover effect over event card
+let eventCardhover = document.querySelector('#backbtn');
+let eventcard = document.querySelector('#eventcardpic');
+document.addEventListener("DOMContentLoaded",function(){
+	eventCardhover.style.display='none';
+})
+eventcard.addEventListener("mouseover", ()=>{
+	eventCardhover.style.display="block";
+})
+eventcard.addEventListener("mouseout", ()=>{
+	eventCardhover.style.display="none";
+})
+                      // 2
+let eventCardhover2 = document.querySelector('#backbtn2');
+let eventcard2 = document.querySelector('#eventcardpic2');
+document.addEventListener("DOMContentLoaded",function(){
+	eventCardhover2.style.display='none';
+})
+eventcard2.addEventListener("mouseover", ()=>{
+	eventCardhover2.style.display="block";
+})
+eventcard2.addEventListener("mouseout", ()=>{
+	eventCardhover2.style.display="none";
+})
+
 // this is for the event page
 const event = document.getElementById('eventparA');
 setTimeout(() => event.style.filter="blur(0)",2000)
+
